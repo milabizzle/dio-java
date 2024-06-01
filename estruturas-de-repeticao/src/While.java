6adff2b50c86627caab5396a59e9006f5763019a
milabizzle <parcerias.cr@gmail.com>
2024-05-31 23:16:22 -0300

    Testando o uso do While

---
+++ estruturas-de-repeticao/src/While.java
@@ -0,0 +1,19 @@
+public class While {
+    public static void main(String[] args) {
+        
+        String clientes [] = {"01", "02","03","04","05"};
+
+        for (String cliente : clientes) {
+            System.out.println("O cliente " + cliente + " está sendo solicitado na sala 2");
+        }
+        /* 
+        do {
+            System.out.println("O cliente " + clientes + " pode prosseguir");
+            
+        } while(clientes == "05")
+        System.out.println("O médico não poderá mais atendê-lo");
+
+        break;*/
+
+    }
+}
