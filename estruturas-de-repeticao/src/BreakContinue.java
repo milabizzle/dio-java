859bb9ce2212a29357caea4b531b5d4580a887d3
milabizzle <parcerias.cr@gmail.com>
2024-05-31 22:42:16 -0300

    Uso do Break/Continue

---
+++ estruturas-de-repeticao/src/BreakContinue.java
@@ -0,0 +1,11 @@
+public class BreakContinue {
+    public static void main(String[] args) {
+        for(int numero = 1; numero <=5; numero ++){
+            if(numero==3)
+                break; //o continue diz pra aplicação não parar naquele "local" e seguir em frente
+            
+            System.out.println(numero);
+            
+        }
+    }
+}
